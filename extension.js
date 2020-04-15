a8af02a736b7e2dfbdef40c3032f3347193c46b9
JC Gonzalez <devgonzalez25@gmail.com>
2020-04-14 19:25:14 -0500

    On command will print out currentSelection variable. This is a object that has the currently highlighted positions. Next step is to get the values in that current positions

--- extension.js
+++ extension.js
@@ -10,18 +10,14 @@ const vscode = require('vscode');
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "opentext" is now active!');
+	let Window = vscode.window;
+	let CurrentSelection;
+	Window.onDidChangeTextEditorSelection(
+		({selections})=>{CurrentSelection=selections}
+	);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('extension.showShorthand', async function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World !');
+		console.log(CurrentSelection);
 	});
 
 	context.subscriptions.push(disposable);

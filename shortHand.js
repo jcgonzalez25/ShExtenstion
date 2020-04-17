1d36ca5c8f20315e1e16424a7f6df89ea3017ff0
JC Gonzalez <jgonzalezriv@liaison.prod>
2020-04-17 14:25:00 -0500

    adding the xml tags to highlighted data

--- shortHand.js
+++ shortHand.js
@@ -15,6 +15,7 @@ module.exports = {
     },
     convert:(data)=>{
         try{
+            let data = "<?xml version=\"1.0\"?><ShortHandXML><Elements><Element>"+data+"</Element></Elements></ShortHandXML>";
             let PostOptions = {
                 hostname: '10.0.24.236',
                 method:"GET",
